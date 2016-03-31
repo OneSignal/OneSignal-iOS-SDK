159903af5b8cc620d2558cae072dc972cf892459
Kasten <josh@onesignal.com>
2016-03-31 11:54:07 -0700

    Set source version to match the release version.

--- iOS_SDK/OneSignal/OneSignal.m
+++ iOS_SDK/OneSignal/OneSignal.m
@@ -61,7 +61,7 @@ static ONE_S_LOG_LEVEL _visualLogLevel = ONE_S_LL_NONE;
 
 @implementation OneSignal
 
-NSString* const ONESIGNAL_VERSION = @"011201";
+NSString* const ONESIGNAL_VERSION = @"011202";
 
 @synthesize app_id = _GT_publicKey;
 @synthesize httpClient = _GT_httpRequest;
