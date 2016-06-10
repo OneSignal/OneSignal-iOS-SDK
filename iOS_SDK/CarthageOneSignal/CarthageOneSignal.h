4a22b311b917a5d5d39435580b64057bc04b95b5
Dirko Swanepoel <dirkos@property24.com>
2016-06-10 11:35:15 +0200

    Carthage

---
+++ iOS_SDK/CarthageOneSignal/CarthageOneSignal.h
@@ -0,0 +1,19 @@
+//
+//  CarthageOneSignal.h
+//  CarthageOneSignal
+//
+//  Created by Dirko Swanepoel on 2016/06/10.
+//  Copyright © 2016 Hiptic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CarthageOneSignal.
+FOUNDATION_EXPORT double CarthageOneSignalVersionNumber;
+
+//! Project version string for CarthageOneSignal.
+FOUNDATION_EXPORT const unsigned char CarthageOneSignalVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CarthageOneSignal/PublicHeader.h>
+
+#import <OneSignal/OneSignal.h>
