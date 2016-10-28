87c9a136864a16b8a4f6ccdf99ce7c205ef376d3
Kasten <josh@onesignal.com>
2016-10-27 19:40:48 -0700

    Updated archive and podspec for 2.2.0 release

--- iOS_SDK/OneSignal/OneSignal.m
+++ iOS_SDK/OneSignal/OneSignal.m
@@ -77,7 +77,7 @@ NSString * const kOSSettingsKeyInFocusDisplayOption = @"kOSSettingsKeyInFocusDis
 
 @implementation OneSignal
     
-NSString* const ONESIGNAL_VERSION = @"020116";
+NSString* const ONESIGNAL_VERSION = @"020200";
 static NSString* mSDKType = @"native";
 static BOOL coldStartFromTapOnNotification = NO;
 static BOOL registeredWithApple = NO; //Has attempted to register for push notifications with Apple.
