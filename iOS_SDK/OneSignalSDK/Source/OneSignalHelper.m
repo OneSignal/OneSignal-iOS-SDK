b36751e82fdf8ecb2c76bf00a7849313346d0e68
Kasten <josh@onesignal.com>
2017-03-31 15:30:13 -0700

    Fixed compile warning with initWithRequest.

--- iOS_SDK/OneSignalSDK/Source/OneSignalHelper.m
+++ iOS_SDK/OneSignalSDK/Source/OneSignalHelper.m
@@ -105,8 +105,7 @@
     NSMutableURLRequest* request = [[NSMutableURLRequest alloc] initWithURL:url];
     
     DirectDownloadDelegate* delegate = [[DirectDownloadDelegate alloc] initWithFilePath:localPath];
-    // TODO: Try cleaning up warning with connectionWithRequest
-    [[NSURLConnection alloc] initWithRequest:request delegate:delegate];
+    [NSURLConnection connectionWithRequest:request delegate:delegate];
     
     while ([delegate isDone] == NO) {
         [[NSRunLoop currentRunLoop] runUntilDate:[NSDate dateWithTimeIntervalSinceNow:1.0]];
