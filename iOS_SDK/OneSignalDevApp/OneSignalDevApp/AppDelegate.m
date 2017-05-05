8cd8a89894e549828f9e824dbef3f514a73aed95
Josh Kasten <josh@onesignal.com>
2017-05-05 16:58:18 -0700

    Added locks to lastLocation to prevent race conditions

--- iOS_SDK/OneSignalDevApp/OneSignalDevApp/AppDelegate.m
+++ iOS_SDK/OneSignalDevApp/OneSignalDevApp/AppDelegate.m
@@ -49,8 +49,10 @@
     [OneSignal initWithLaunchOptions:launchOptions
                                appId:@"b2f7f966-d8cc-11e4-bed1-df8f05be55ba"
             handleNotificationAction:openNotificationHandler
-                            settings:@{kOSSettingsKeyAutoPrompt: @false}];
+                            settings:@{kOSSettingsKeyAutoPrompt: @false,
+                                       kOSSettingsKeyInAppLaunchURL: @false}];
     
+    [OneSignal promptLocation];
     [OneSignal sendTag:@"someKey1122" value:@"03222017"];
     
     [OneSignal addPermissionObserver:self];
