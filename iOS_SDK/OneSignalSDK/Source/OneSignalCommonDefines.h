3bb0d4949d62d4579d24ee04943b842ec72f1888
Brad Hesse <nightsd01@gmail.com>
2018-02-09 11:28:35 -0800

    Update Server URL
    • Mistakenly committed staging server as the OneSignal server URL in common defines

--- iOS_SDK/OneSignalSDK/Source/OneSignalCommonDefines.h
+++ iOS_SDK/OneSignalSDK/Source/OneSignalCommonDefines.h
@@ -11,7 +11,7 @@
 
 // networking
 #define API_VERSION @"api/v1/"
-#define SERVER_URL @"https://onesignal-staging.pw/"
+#define SERVER_URL @"https://onesignal.com/"
 
 // NSUserDefaults parameter names
 #define EMAIL_AUTH_CODE @"GT_EMAIL_AUTH_CODE"
