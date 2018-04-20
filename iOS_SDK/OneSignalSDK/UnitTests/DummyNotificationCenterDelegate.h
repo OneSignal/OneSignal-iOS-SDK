acbfd28c58858fc9c484864a8e47c7c2e7d51abf
Brad Hesse <nightsd01@gmail.com>
2018-04-20 13:11:13 -0700

    Swizzling (#364)
    * Swizzling
    
    • Ensures that UNNotificationCenter setDelegate: will never get called twice, which is useful if other SDK's are swizzling the same method incorrectly in a way that invites race conditions
    
    * Add Test
    
    • Adds a test to make sure that calling UNUserNotificationCenter +setDelegate: does not swizzle the same object twice.
    
    * Add Comment
    
    • Adds a comment to explain what is happening in the registerAsUNNotificationCenterDelegate() method which looks a bit confusing

---
+++ iOS_SDK/OneSignalSDK/UnitTests/DummyNotificationCenterDelegate.h
@@ -0,0 +1,14 @@
+//
+//  DummyNotificationCenterDelegate.h
+//  UnitTests
+//
+//  Created by Brad Hesse on 4/19/18.
+//  Copyright © 2018 Hiptic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UserNotifications/UserNotifications.h>
+
+@interface DummyNotificationCenterDelegate : NSObject <UNUserNotificationCenterDelegate>
+
+@end
