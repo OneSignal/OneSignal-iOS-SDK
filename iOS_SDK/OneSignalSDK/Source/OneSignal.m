a8c13381037f7aad4dc0405ec0e4da7191458684
Brad Hesse <nightsd01@gmail.com>
2018-04-29 09:40:34 -0700

    2.8.4 Release
    • Fixes a private API issues preventing developers from uploading binaries to the App Store
    • Any methods named valueForQueryParameter() are apparently flagged by Apple as private API usage......
    • Changed the name of the method so it no longer triggers this issue.

--- iOS_SDK/OneSignalSDK/Source/OneSignal.m
+++ iOS_SDK/OneSignalSDK/Source/OneSignal.m
@@ -132,7 +132,7 @@ NSString* const kOSSSettingsKeyPromptBeforeOpeningPushURL = @"kOSSSettingsKeyPro
 
 @implementation OneSignal
 
-NSString* const ONESIGNAL_VERSION = @"020803";
+NSString* const ONESIGNAL_VERSION = @"020804";
 static NSString* mSDKType = @"native";
 static BOOL coldStartFromTapOnNotification = NO;
 
