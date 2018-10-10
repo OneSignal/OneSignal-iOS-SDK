46b8388af6ed3ef031121fb319537d027cf8131c
Brad Hesse <nightsd01@gmail.com>
2018-10-10 11:37:07 -0700

    Fix Deliver Quietly
    • Our SDK was not detecting the 'deliver quietly' option of iOS 12 in some conditions
    • This fix adds a notificationTypes check so that our backend does not assume the device has notifications disabled

--- iOS_SDK/OneSignalSDK/Source/OneSignalNotificationSettingsIOS10.m
+++ iOS_SDK/OneSignalSDK/Source/OneSignalNotificationSettingsIOS10.m
@@ -80,9 +80,13 @@ static dispatch_queue_t serialQueue;
                                      + (settings.alertSetting == UNNotificationSettingEnabled ? 4 : 0)
                                      + (settings.lockScreenSetting == UNNotificationSettingEnabled ? 8 : 0);
             
-            if ([OneSignalHelper isIOSVersionGreaterOrEqual:12.0])
-                if (status.notificationTypes == 0 && settings.authorizationStatus == provisionalStatus)
-                    status.notificationTypes = PROVISIONAL_UNAUTHORIZATIONOPTION;
+            // check if using provisional notifications
+            // otherwise 'deliver quietly' is enabled.
+            
+            if ([OneSignalHelper isIOSVersionGreaterOrEqual:12.0] && status.notificationTypes == 0 && settings.authorizationStatus == provisionalStatus)
+                status.notificationTypes = PROVISIONAL_UNAUTHORIZATIONOPTION;
+            else if ([OneSignalHelper isIOSVersionGreaterOrEqual:10.0] && settings.notificationCenterSetting == UNNotificationSettingEnabled)
+                status.notificationTypes += 16;
             
             self.useCachedStatus = true;
             completionHandler(status);
