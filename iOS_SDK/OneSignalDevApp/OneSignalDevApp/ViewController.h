19734fae324b2d60cb5b320c9fa1bed6a5ed337a
Brad Hesse <nightsd01@gmail.com>
2018-12-03 17:18:23 -0800

    Add External ID To Demo Project
    • We have a (very) simple dev project for the SDK. This commit adds a text field and button to set an arbitrary external User ID, as well as a button to remove the ID

--- iOS_SDK/OneSignalDevApp/OneSignalDevApp/ViewController.h
+++ iOS_SDK/OneSignalDevApp/OneSignalDevApp/ViewController.h
@@ -30,7 +30,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UITextFieldDelegate>
 
 
 @end
--- iOS_SDK/OneSignalDevApp/OneSignalDevApp/ViewController.m
+++ iOS_SDK/OneSignalDevApp/OneSignalDevApp/ViewController.m
@@ -36,6 +36,7 @@
 @property (weak, nonatomic) IBOutlet UITextField *textField;
 @property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicatorView;
 @property (weak, nonatomic) IBOutlet UISegmentedControl *consentSegmentedControl;
+@property (weak, nonatomic) IBOutlet UITextField *externalIdTextField;
 
 @end
 
@@ -48,6 +49,9 @@
     self.activityIndicatorView.hidden = true;
     
     self.consentSegmentedControl.selectedSegmentIndex = (NSInteger)![OneSignal requiresUserPrivacyConsent];
+    
+    self.textField.delegate = self;
+    self.externalIdTextField.delegate = self;
 }
 
 - (void)changeAnimationState:(BOOL)animating {
@@ -129,5 +133,18 @@
     [OneSignal consentGranted:(bool)sender.selectedSegmentIndex];
 }
 
+- (IBAction)setExternalIdButtonPressed:(UIButton *)sender {
+    [OneSignal setExternalUserId:self.externalIdTextField.text];
+}
+
+- (IBAction)removeExternalIdButtonPressed:(UIButton *)sender {
+    [OneSignal removeExternalUserId];
+}
+
+#pragma mark UITextFieldDelegate Methods
+-(BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [textField resignFirstResponder];
+    return false;
+}
 
 @end
