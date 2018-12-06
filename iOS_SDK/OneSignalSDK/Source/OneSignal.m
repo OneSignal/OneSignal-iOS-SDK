a807ba8f2fe5d0ebffca9d86375eda890560aa18
Brad Hesse <nightsd01@gmail.com>
2018-12-06 12:06:54 -0800

    2.9.4 Release
    • Includes a fix for a rare crash that was caused by invalid HTTP request parameters

--- iOS_SDK/OneSignalSDK/Source/OneSignal.m
+++ iOS_SDK/OneSignalSDK/Source/OneSignal.m
@@ -122,7 +122,7 @@ NSString* const kOSSettingsKeyProvidesAppNotificationSettings = @"kOSSettingsKey
 
 @implementation OneSignal
 
-NSString* const ONESIGNAL_VERSION = @"020903";
+NSString* const ONESIGNAL_VERSION = @"020904";
 static NSString* mSDKType = @"native";
 static BOOL coldStartFromTapOnNotification = NO;
 
