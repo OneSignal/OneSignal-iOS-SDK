29a7813c2b78812ed7ee92c8850dfc7789a7320c
Josh Kasten <josh@onesignal.com>
2019-12-03 14:06:48 -0800

    Release commit 2.12.1

--- iOS_SDK/OneSignalSDK/Source/OneSignal.m
+++ iOS_SDK/OneSignalSDK/Source/OneSignal.m
@@ -132,7 +132,7 @@ NSString* const kOSSettingsKeyProvidesAppNotificationSettings = @"kOSSettingsKey
 
 @implementation OneSignal
 
-NSString* const ONESIGNAL_VERSION = @"021200";
+NSString* const ONESIGNAL_VERSION = @"021201";
 static NSString* mSDKType = @"native";
 static BOOL coldStartFromTapOnNotification = NO;
 
