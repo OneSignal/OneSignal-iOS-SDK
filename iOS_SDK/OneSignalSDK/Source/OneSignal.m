ddf63b29c405e2620bc5d8b88e1640eb6f94551c
Michael DiCioccio <mdicioccio25@gmail.com>
2020-04-02 15:50:34 -0700

    Updated binary and header file and incremented version to 2.13.1 (#653)

--- iOS_SDK/OneSignalSDK/Source/OneSignal.m
+++ iOS_SDK/OneSignalSDK/Source/OneSignal.m
@@ -132,7 +132,7 @@ NSString* const kOSSettingsKeyProvidesAppNotificationSettings = @"kOSSettingsKey
 
 @implementation OneSignal
 
-NSString* const ONESIGNAL_VERSION = @"021300";
+NSString* const ONESIGNAL_VERSION = @"021301";
 static NSString* mSDKType = @"native";
 static BOOL coldStartFromTapOnNotification = NO;
 
