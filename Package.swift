e3a27a909f7989eab8e7a0728b5580a397ebb521
emawby <emawby@gmail.com>
2020-05-21 16:30:12 -0700

    Supporting Swift package manager and removing unneeded files

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "OneSignal",
+    products: [
+        .library(
+            name: "OneSignal",
+            type: .static,
+            targets: ["OneSignal"]),
+    ],
+    targets: [
+        .target(
+            name: "OneSignal",
+            dependencies: [],
+            path: "iOS_SDK/OneSignalSDK/",
+            sources: ["Source"],
+            publicHeadersPath:"Source"),
+    ]
+)
+
