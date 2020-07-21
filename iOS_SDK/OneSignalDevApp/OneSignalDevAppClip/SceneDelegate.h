b9b83db1455f082345020e170607bcde837a2475
emawby <emawby@gmail.com>
2020-07-21 12:00:27 -0700

    Creating test App Clip

---
+++ iOS_SDK/OneSignalDevApp/OneSignalDevAppClip/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  OneSignalDevAppClip
+//
+//  Created by Elliot Mawby on 7/21/20.
+//  Copyright © 2020 OneSignal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
