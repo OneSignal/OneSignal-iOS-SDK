9a656cf4c721d00db07e71eefe4023817ca77b0d
emawby <emawby@gmail.com>
2020-09-24 15:02:12 -0700

    updating objc subscription label

--- Examples/ObjectiveCExample/OneSignalDemo/ViewController.m
+++ Examples/ObjectiveCExample/OneSignalDemo/ViewController.m
@@ -300,11 +300,11 @@
 -(void)onOSSubscriptionChanged:(OSSubscriptionStateChanges *)stateChanges {
     if (stateChanges.from.subscribed &&  !stateChanges.to.subscribed) {
         self.subscriptionStatusSwitch.on = false;
-        self.subscriptionStatusLabel.text = @"Set Subscription OFF";
+        self.subscriptionStatusLabel.text = @"OneSignal Push Disabled";
         self.registerButton.backgroundColor = [UIColor redColor];
     } else if (!stateChanges.from.subscribed && stateChanges.to.subscribed) {
         self.subscriptionStatusSwitch.on = true;
-        self.subscriptionStatusLabel.text = @"Set Subscription ON";
+        self.subscriptionStatusLabel.text = @"OneSignal Push Enabled";
         self.registerButton.backgroundColor = [UIColor greenColor];
     }
 }
