36cad6a81b52b22b47059b43818183a7c1d3af32
emawby <emawby@gmail.com>
2020-12-16 11:34:18 -0800

    add arm64 slice to simulator build
    In order to support Apple Silicon macs we need to add arm64 to
    iOS simulator builds. This only works with XCFrameworks since we
    also need arm64 for iOS devices.

--- iOS_SDK/OneSignalSDK/build_xcframework.sh
+++ iOS_SDK/OneSignalSDK/build_xcframework.sh
@@ -23,7 +23,7 @@ mkdir "${FRAMEWORK_FOLDER_NAME}"
 echo "Created ${FRAMEWORK_FOLDER_NAME}"
 echo "Archiving ${FRAMEWORK_NAME}"
 
-xcodebuild archive -scheme ${BUILD_SCHEME} -destination="iOS Simulator" -archivePath "${SIMULATOR_ARCHIVE_PATH}" -sdk iphonesimulator SKIP_INSTALL=NO BUILD_LIBRARIES_FOR_DISTRIBUTION=YES
+xcodebuild archive ONLY_ACTIVE_ARCH=NO -scheme ${BUILD_SCHEME} -destination="iOS Simulator" -arch i386 -arch x86_64 -arch arm64 -archivePath "${SIMULATOR_ARCHIVE_PATH}" -sdk iphonesimulator SKIP_INSTALL=NO BUILD_LIBRARIES_FOR_DISTRIBUTION=YES
 
 xcodebuild archive -scheme ${BUILD_SCHEME} -destination="iOS" -archivePath "${IOS_DEVICE_ARCHIVE_PATH}" -sdk iphoneos SKIP_INSTALL=NO BUILD_LIBRARIES_FOR_DISTRIBUTION=YES
 
