8cf9560e22d15ea0363502de2f075ea25b34f027
emawby <emawby@gmail.com>
2020-12-17 09:25:38 -0800

    updating SwiftPM to distribute a binary target
    Additionally we are updating build_xcframework.sh to
    automatically archive the XCFramework and update the Swift.package
    Checksum to the checksum corresponding to the new archive. It also
    asks for the new version number and updates the URL in the Swift
    package to correspond to the new release.
    Note that this uses a 'dumb' SED command that replaces line 16 and 17 of
    Swift.package so changes to that file could break this command.

--- iOS_SDK/OneSignalSDK/build_xcframework.sh
+++ iOS_SDK/OneSignalSDK/build_xcframework.sh
@@ -9,6 +9,8 @@ FRAMEWORK_NAME="OneSignal"
 
 FRAMEWORK_PATH="${WORKING_DIR}/${FRAMEWORK_FOLDER_NAME}/${FRAMEWORK_NAME}.xcframework"
 
+FRAMEWORK_ZIP_PATH="${WORKING_DIR}/${FRAMEWORK_FOLDER_NAME}/${FRAMEWORK_NAME}.xcframework.zip"
+
 BUILD_SCHEME="OneSignalFramework"
 
 SIMULATOR_ARCHIVE_PATH="${WORKING_DIR}/${FRAMEWORK_FOLDER_NAME}/simulator.xcarchive"
@@ -16,6 +18,8 @@ SIMULATOR_ARCHIVE_PATH="${WORKING_DIR}/${FRAMEWORK_FOLDER_NAME}/simulator.xcarch
 IOS_DEVICE_ARCHIVE_PATH="${WORKING_DIR}/${FRAMEWORK_FOLDER_NAME}/iOS.xcarchive"
 
 CATALYST_ARCHIVE_PATH="${WORKING_DIR}/${FRAMEWORK_FOLDER_NAME}/catalyst.xcarchive"
+
+SWIFT_PACKAGE_PATH="../../Package.swift"
 
 rm -rf "${WORKING_DIR}/${FRAMEWORK_FOLDER_NAME}"
 echo "Deleted ${FRAMEWORK_FOLDER_NAME}"
@@ -30,8 +34,24 @@ xcodebuild archive -scheme ${BUILD_SCHEME} -destination="iOS" -archivePath "${IO
 xcodebuild archive -scheme ${BUILD_SCHEME} -destination='platform=macOS,arch=x86_64,variant=Mac Catalyst' -archivePath "${CATALYST_ARCHIVE_PATH}" SKIP_INSTALL=NO BUILD_LIBRARIES_FOR_DISTRIBUTION=YES
 
 xcodebuild -create-xcframework -framework ${SIMULATOR_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework -framework ${IOS_DEVICE_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework -framework ${CATALYST_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework -output "${FRAMEWORK_PATH}"
+# Remove the old Zipped XCFramework and create a new Zip
+rm -rf "${FRAMEWORK_ZIP_PATH}"
+ditto -c -k --sequesterRsrc --keepParent "${FRAMEWORK_PATH}" "${FRAMEWORK_ZIP_PATH}" 
 
+# Compute the checksum for the Zipped framework
+CHECKSUM=$(swift package compute-checksum "${FRAMEWORK_ZIP_PATH}")
+SWIFT_PM_CHECKSUM_LINE="          checksum: \"${CHECKSUM}\""
+# Use sed to remove line 17 from the Swift.package and replace it with the new checksum
+sed -i '' "17s/.*/$SWIFT_PM_CHECKSUM_LINE/" "${SWIFT_PACKAGE_PATH}"
+#Ask for the new release version number to be placed in the package URL
+echo -e "\033[1mEnter the new SDK release version number\033[0m"
+read VERSION_NUMBER
+SWIFT_PM_URL_LINE="          url: \"https:\/\/github.com\/OneSignal\/OneSignal-iOS-SDK\/releases\/download\/${VERSION_NUMBER}\/OneSignal.xcframework.zip\","
+# Use sed to remove line 16 from the Swift.package and replace it with the new URL for the new release
+sed -i '' "16s/.*/$SWIFT_PM_URL_LINE/" "${SWIFT_PACKAGE_PATH}"
+#Open XCFramework folder to drag zip into new release
 rm -rf "${SIMULATOR_ARCHIVE_PATH}"
 rm -rf "${IOS_DEVICE_ARCHIVE_PATH}"
 rm -rf "${CATALYST_ARCHIVE_PATH}"
 open "${WORKING_DIR}/${FRAMEWORK_FOLDER_NAME}"
+
