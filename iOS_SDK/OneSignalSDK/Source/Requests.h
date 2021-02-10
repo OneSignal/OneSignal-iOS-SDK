3e9a57aaa82e2cedea07d3cdf4e36d252243e086
Jeasmine Nahui <jeas.nahui@gmail.com>
2021-02-10 20:14:35 -0300

    Add SMS location request support
    * Add auth hash and external id support for location request SMS channel request

--- iOS_SDK/OneSignalSDK/Source/Requests.h
+++ iOS_SDK/OneSignalSDK/Source/Requests.h
@@ -96,6 +96,8 @@ NS_ASSUME_NONNULL_END
 
 @interface OSRequestSendLocation : OneSignalRequest
 + (instancetype _Nonnull)withUserId:(NSString * _Nonnull)userId appId:(NSString * _Nonnull)appId location:(os_last_location * _Nonnull)coordinate networkType:(NSNumber * _Nonnull)netType backgroundState:(BOOL)backgroundState emailAuthHashToken:(NSString * _Nullable)emailAuthHash externalIdAuthToken:(NSString * _Nullable)externalIdAuthToken;
+
++ (instancetype _Nonnull)withUserId:(NSString * _Nonnull)userId appId:(NSString * _Nonnull)appId location:(os_last_location * _Nonnull)coordinate networkType:(NSNumber * _Nonnull)netType backgroundState:(BOOL)backgroundState smsAuthHashToken:(NSString * _Nullable)smsAuthHash externalIdAuthToken:(NSString * _Nullable)externalIdAuthToken;
 @end
 
 @interface OSRequestBadgeCount : OneSignalRequest
