1260679a1a2d899ea86a06db8e9e73cc489ae468
Jeasmine Nahui <jeas.nahui@gmail.com>
2021-02-12 20:17:28 -0300

    Fix SMSTests File
    * Remove Target Membership OneSignal and Framework

--- iOS_SDK/OneSignalSDK/Source/OneSignal.m
+++ iOS_SDK/OneSignalSDK/Source/OneSignal.m
@@ -243,7 +243,7 @@ static OSEmailSubscriptionState* _currentEmailSubscriptionState;
 + (OSEmailSubscriptionState *)currentEmailSubscriptionState {
     if (!_currentEmailSubscriptionState) {
         _currentEmailSubscriptionState = [[OSEmailSubscriptionState alloc] init];
-        
+
         [_currentEmailSubscriptionState.observable addObserver:[OSEmailSubscriptionChangedInternalObserver alloc]];
     }
     return _currentEmailSubscriptionState;
@@ -487,7 +487,7 @@ static OneSignalOutcomeEventsController *_outcomeEventsController;
     
     //call persistAsFrom in order to save the emailAuthToken & playerId to NSUserDefaults
     [self.currentEmailSubscriptionState persist];
-    
+
     [OneSignal onesignal_Log:ONE_S_LL_VERBOSE message:[NSString stringWithFormat:@"CurrentEmailSubscriptionState after saveEmailAddress: %@", self.currentEmailSubscriptionState.description]];
 }
 
@@ -504,10 +504,10 @@ static OneSignalOutcomeEventsController *_outcomeEventsController;
     [self.currentSMSSubscriptionState setSMSNumber:smsNumber];
     self.currentSMSSubscriptionState.smsAuthCode = smsAuthToken;
     [self.currentSMSSubscriptionState setSMSUserId:smsPlayerId];
-    
+
     //call persistAsFrom in order to save the smsNumber & playerId to NSUserDefaults
     [self.currentSMSSubscriptionState persist];
-    
+
     [OneSignal onesignal_Log:ONE_S_LL_VERBOSE message:[NSString stringWithFormat:@"CurrentSMSSubscriptionState after saveSMSNumber: %@", self.currentSMSSubscriptionState.description]];
 }
 
@@ -1964,7 +1964,7 @@ static dispatch_queue_t serialQueue;
             
             if (self.currentSubscriptionState.delayedObserverUpdate)
                 [self.currentSubscriptionState setAccepted:[self getNotificationTypes] > 0];
-            
+
         } onFailure:nil];
 
         return true;
@@ -2493,7 +2493,7 @@ static NSString *_lastnonActiveMessageId;
     self.currentEmailSubscriptionState.emailUserId = emailPlayerId;
     
     [self.currentEmailSubscriptionState persist];
-    
+
     let request = [OSRequestUpdateDeviceToken withUserId:self.currentSubscriptionState.userId
                                                    appId:self.appId
                                                    deviceToken:nil
