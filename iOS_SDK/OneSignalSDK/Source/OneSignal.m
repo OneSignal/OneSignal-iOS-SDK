7733264ec63399265ba7c886e2c28d6b814cec85
emawby <emawby@gmail.com>
2021-02-16 14:57:34 -0800

    bumping version to 3.2.1

--- iOS_SDK/OneSignalSDK/Source/OneSignal.m
+++ iOS_SDK/OneSignalSDK/Source/OneSignal.m
@@ -124,7 +124,7 @@ NSString* const kOSSettingsKeyProvidesAppNotificationSettings = @"kOSSettingsKey
 
 @implementation OneSignal
 
-NSString* const ONESIGNAL_VERSION = @"030200";
+NSString* const ONESIGNAL_VERSION = @"030201";
 static NSString* mSDKType = @"native";
 static BOOL coldStartFromTapOnNotification = NO;
 static BOOL shouldDelaySubscriptionUpdate = false;
