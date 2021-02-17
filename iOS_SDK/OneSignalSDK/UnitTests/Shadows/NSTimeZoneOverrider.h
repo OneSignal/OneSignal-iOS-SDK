dd969732f401ac28c513f5ddc638ddb5984ecb01
Gonzalo Narbaiz <gonzalo@onesignal.com>
2021-02-17 18:45:11 -0300

    Added NSTimeZoneOverrider to mock Timezone value

---
+++ iOS_SDK/OneSignalSDK/UnitTests/Shadows/NSTimeZoneOverrider.h
@@ -0,0 +1,13 @@
+//
+//  NSTimeZoneOverrider.h
+//  OneSignal
+//
+//  Created by Gonzalo Narbaiz on 17/02/2021.
+//  Copyright © 2021 Hiptic. All rights reserved.
+//
+
+#ifndef NSTimeZoneOverrider_h
+#define NSTimeZoneOverrider_h
+
+
+#endif /* NSTimeZoneOverrider_h */
---
+++ iOS_SDK/OneSignalSDK/UnitTests/Shadows/NSTimeZoneOverrider.m
@@ -0,0 +1,9 @@
+//
+//  NSTimeZoneOverrider.m
+//  UnitTests
+//
+//  Created by Gonzalo Narbaiz on 17/02/2021.
+//  Copyright © 2021 Hiptic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
