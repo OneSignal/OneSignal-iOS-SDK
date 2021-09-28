48912f16387c622667679dcedb680ecaa6e6d258
emawby <emawby@gmail.com>
2021-09-28 09:55:50 -0700

    Creating OneSignalOutcomes framework
    Moving Outcomes files to new framework
    
    This framework will also contain sessions

---
+++ iOS_SDK/OneSignalSDK/OneSignalOutcomes/OneSignalOutcomes.h
@@ -0,0 +1,19 @@
+//
+//  OneSignalOutcomes.h
+//  OneSignalOutcomes
+//
+//  Created by Elliot Mawby on 9/28/21.
+//  Copyright © 2021 Hiptic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OneSignalOutcomes.
+FOUNDATION_EXPORT double OneSignalOutcomesVersionNumber;
+
+//! Project version string for OneSignalOutcomes.
+FOUNDATION_EXPORT const unsigned char OneSignalOutcomesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OneSignalOutcomes/PublicHeader.h>
+
+
