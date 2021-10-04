ea958d0d520c4bfadce9a7d427eaf6a6dc8435bc
emawby <emawby@gmail.com>
2021-10-04 13:13:48 -0700

    Working on outcomes unittests
    There are still flaky outcomes tests, and the migration test is failing because we aren't migrating on notification received from the NSE

--- iOS_SDK/OneSignalSDK/OneSignalOutcomes/OneSignalOutcomes.h
+++ iOS_SDK/OneSignalSDK/OneSignalOutcomes/OneSignalOutcomes.h
@@ -14,6 +14,7 @@
 #import "OSInAppMessageOutcome.h"
 #import "OSOutcomeEvent.h"
 #import "OSInfluenceDataDefines.h"
+#import "OSOutcomeEventsCache.h"
 // In this header, you should import all the public headers of your framework using statements like #import <OneSignalOutcomes/PublicHeader.h>
 
 
