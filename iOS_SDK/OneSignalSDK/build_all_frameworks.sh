5e2b14c439eb544a7c56978cd7205513a873985f
emawby <emawby@gmail.com>
2021-12-09 11:35:20 -0800

    cleaning up build_all_frameworks script to use 1 function

--- iOS_SDK/OneSignalSDK/build_all_frameworks.sh
+++ iOS_SDK/OneSignalSDK/build_all_frameworks.sh
@@ -3,136 +3,50 @@ set -e
 
 WORKING_DIR=$(pwd)
 
+create_xcframework() {
+    FRAMEWORK_FOLDER_NAME=$1
+
+    FRAMEWORK_NAME=$2
+
+    FRAMEWORK_PATH="${WORKING_DIR}/${FRAMEWORK_FOLDER_NAME}/${FRAMEWORK_NAME}.xcframework"
+
+    BUILD_SCHEME=$3
+
+    SIMULATOR_ARCHIVE_PATH="${WORKING_DIR}/${FRAMEWORK_FOLDER_NAME}/simulator.xcarchive"
+
+    IOS_DEVICE_ARCHIVE_PATH="${WORKING_DIR}/${FRAMEWORK_FOLDER_NAME}/iOS.xcarchive"
+
+    CATALYST_ARCHIVE_PATH="${WORKING_DIR}/${FRAMEWORK_FOLDER_NAME}/catalyst.xcarchive"
+
+    rm -rf "${WORKING_DIR}/${FRAMEWORK_FOLDER_NAME}"
+    echo "Deleted ${FRAMEWORK_FOLDER_NAME}"
+    mkdir "${FRAMEWORK_FOLDER_NAME}"
+    echo "Created ${FRAMEWORK_FOLDER_NAME}"
+    echo "Archiving ${FRAMEWORK_NAME}"
+
+    xcodebuild archive ONLY_ACTIVE_ARCH=NO -scheme ${BUILD_SCHEME} -destination="generic/platform=iOS Simulator" -archivePath "${SIMULATOR_ARCHIVE_PATH}" -sdk iphonesimulator SKIP_INSTALL=NO BUILD_LIBRARIES_FOR_DISTRIBUTION=YES
+
+    xcodebuild archive -scheme ${BUILD_SCHEME} -destination="generic/platform=iOS" -archivePath "${IOS_DEVICE_ARCHIVE_PATH}" -sdk iphoneos SKIP_INSTALL=NO BUILD_LIBRARIES_FOR_DISTRIBUTION=YES
+
+    xcodebuild archive -scheme ${BUILD_SCHEME} -destination='generic/platform=macOS,variant=Mac Catalyst' -archivePath "${CATALYST_ARCHIVE_PATH}" SKIP_INSTALL=NO BUILD_LIBRARIES_FOR_DISTRIBUTION=YES
+
+    xcodebuild -create-xcframework -framework ${SIMULATOR_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework -framework ${IOS_DEVICE_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework -framework ${CATALYST_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework -output "${FRAMEWORK_PATH}"
+
+    rm -rf "${SIMULATOR_ARCHIVE_PATH}"
+    rm -rf "${IOS_DEVICE_ARCHIVE_PATH}"
+    rm -rf "${CATALYST_ARCHIVE_PATH}"
+}
+
 ## BUILD ONESIGNAL CORE ##
-FRAMEWORK_FOLDER_NAME="OneSignal_Core"
-
-FRAMEWORK_NAME="OneSignalCore"
-
-FRAMEWORK_PATH="${WORKING_DIR}/${FRAMEWORK_FOLDER_NAME}/${FRAMEWORK_NAME}.xcframework"
-
-BUILD_SCHEME="OneSignalCore"
-
-SIMULATOR_ARCHIVE_PATH="${WORKING_DIR}/${FRAMEWORK_FOLDER_NAME}/simulator.xcarchive"
-
-IOS_DEVICE_ARCHIVE_PATH="${WORKING_DIR}/${FRAMEWORK_FOLDER_NAME}/iOS.xcarchive"
-
-CATALYST_ARCHIVE_PATH="${WORKING_DIR}/${FRAMEWORK_FOLDER_NAME}/catalyst.xcarchive"
-
-rm -rf "${WORKING_DIR}/${FRAMEWORK_FOLDER_NAME}"
-echo "Deleted ${FRAMEWORK_FOLDER_NAME}"
-mkdir "${FRAMEWORK_FOLDER_NAME}"
-echo "Created ${FRAMEWORK_FOLDER_NAME}"
-echo "Archiving ${FRAMEWORK_NAME}"
-
-xcodebuild archive ONLY_ACTIVE_ARCH=NO -scheme ${BUILD_SCHEME} -destination="generic/platform=iOS Simulator" -archivePath "${SIMULATOR_ARCHIVE_PATH}" -sdk iphonesimulator SKIP_INSTALL=NO BUILD_LIBRARIES_FOR_DISTRIBUTION=YES
-
-xcodebuild archive -scheme ${BUILD_SCHEME} -destination="generic/platform=iOS" -archivePath "${IOS_DEVICE_ARCHIVE_PATH}" -sdk iphoneos SKIP_INSTALL=NO BUILD_LIBRARIES_FOR_DISTRIBUTION=YES
-
-xcodebuild archive -scheme ${BUILD_SCHEME} -destination='generic/platform=macOS,variant=Mac Catalyst' -archivePath "${CATALYST_ARCHIVE_PATH}" SKIP_INSTALL=NO BUILD_LIBRARIES_FOR_DISTRIBUTION=YES
-
-xcodebuild -create-xcframework -framework ${SIMULATOR_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework -framework ${IOS_DEVICE_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework -framework ${CATALYST_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework -output "${FRAMEWORK_PATH}"
-
-rm -rf "${SIMULATOR_ARCHIVE_PATH}"
-rm -rf "${IOS_DEVICE_ARCHIVE_PATH}"
-rm -rf "${CATALYST_ARCHIVE_PATH}"
+create_xcframework "OneSignal_Core" "OneSignalCore" "OneSignalCore"
 
 ## BUILD ONESIGNAL OUTCOMES ##
-FRAMEWORK_FOLDER_NAME="OneSignal_Outcomes"
-
-FRAMEWORK_NAME="OneSignalOutcomes"
-
-FRAMEWORK_PATH="${WORKING_DIR}/${FRAMEWORK_FOLDER_NAME}/${FRAMEWORK_NAME}.xcframework"
-
-BUILD_SCHEME="OneSignalOutcomes"
-
-SIMULATOR_ARCHIVE_PATH="${WORKING_DIR}/${FRAMEWORK_FOLDER_NAME}/simulator.xcarchive"
-
-IOS_DEVICE_ARCHIVE_PATH="${WORKING_DIR}/${FRAMEWORK_FOLDER_NAME}/iOS.xcarchive"
-
-CATALYST_ARCHIVE_PATH="${WORKING_DIR}/${FRAMEWORK_FOLDER_NAME}/catalyst.xcarchive"
-
-rm -rf "${WORKING_DIR}/${FRAMEWORK_FOLDER_NAME}"
-echo "Deleted ${FRAMEWORK_FOLDER_NAME}"
-mkdir "${FRAMEWORK_FOLDER_NAME}"
-echo "Created ${FRAMEWORK_FOLDER_NAME}"
-echo "Archiving ${FRAMEWORK_NAME}"
-
-xcodebuild archive ONLY_ACTIVE_ARCH=NO -scheme ${BUILD_SCHEME} -destination="generic/platform=iOS Simulator" -archivePath "${SIMULATOR_ARCHIVE_PATH}" -sdk iphonesimulator SKIP_INSTALL=NO BUILD_LIBRARIES_FOR_DISTRIBUTION=YES
-
-xcodebuild archive -scheme ${BUILD_SCHEME} -destination="generic/platform=iOS" -archivePath "${IOS_DEVICE_ARCHIVE_PATH}" -sdk iphoneos SKIP_INSTALL=NO BUILD_LIBRARIES_FOR_DISTRIBUTION=YES
-
-xcodebuild archive -scheme ${BUILD_SCHEME} -destination='generic/platform=macOS,variant=Mac Catalyst' -archivePath "${CATALYST_ARCHIVE_PATH}" SKIP_INSTALL=NO BUILD_LIBRARIES_FOR_DISTRIBUTION=YES
-
-xcodebuild -create-xcframework -framework ${SIMULATOR_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework -framework ${IOS_DEVICE_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework -framework ${CATALYST_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework -output "${FRAMEWORK_PATH}"
-
-rm -rf "${SIMULATOR_ARCHIVE_PATH}"
-rm -rf "${IOS_DEVICE_ARCHIVE_PATH}"
-rm -rf "${CATALYST_ARCHIVE_PATH}"
+create_xcframework "OneSignal_Outcomes" "OneSignalOutcomes" "OneSignalOutcomes"
 
 ## BUILD ONESIGNAL EXTENSION ##
-FRAMEWORK_FOLDER_NAME="OneSignal_Extension"
-
-FRAMEWORK_NAME="OneSignalExtension"
-
-FRAMEWORK_PATH="${WORKING_DIR}/${FRAMEWORK_FOLDER_NAME}/${FRAMEWORK_NAME}.xcframework"
-
-BUILD_SCHEME="OneSignalExtension"
-
-SIMULATOR_ARCHIVE_PATH="${WORKING_DIR}/${FRAMEWORK_FOLDER_NAME}/simulator.xcarchive"
-
-IOS_DEVICE_ARCHIVE_PATH="${WORKING_DIR}/${FRAMEWORK_FOLDER_NAME}/iOS.xcarchive"
-
-CATALYST_ARCHIVE_PATH="${WORKING_DIR}/${FRAMEWORK_FOLDER_NAME}/catalyst.xcarchive"
-
-rm -rf "${WORKING_DIR}/${FRAMEWORK_FOLDER_NAME}"
-echo "Deleted ${FRAMEWORK_FOLDER_NAME}"
-mkdir "${FRAMEWORK_FOLDER_NAME}"
-echo "Created ${FRAMEWORK_FOLDER_NAME}"
-echo "Archiving ${FRAMEWORK_NAME}"
-
-xcodebuild archive ONLY_ACTIVE_ARCH=NO -scheme ${BUILD_SCHEME} -destination="generic/platform=iOS Simulator" -archivePath "${SIMULATOR_ARCHIVE_PATH}" -sdk iphonesimulator SKIP_INSTALL=NO BUILD_LIBRARIES_FOR_DISTRIBUTION=YES
-
-xcodebuild archive -scheme ${BUILD_SCHEME} -destination="generic/platform=iOS" -archivePath "${IOS_DEVICE_ARCHIVE_PATH}" -sdk iphoneos SKIP_INSTALL=NO BUILD_LIBRARIES_FOR_DISTRIBUTION=YES
-
-xcodebuild archive -scheme ${BUILD_SCHEME} -destination='generic/platform=macOS,variant=Mac Catalyst' -archivePath "${CATALYST_ARCHIVE_PATH}" SKIP_INSTALL=NO BUILD_LIBRARIES_FOR_DISTRIBUTION=YES
-
-xcodebuild -create-xcframework -framework ${SIMULATOR_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework -framework ${IOS_DEVICE_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework -framework ${CATALYST_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework -output "${FRAMEWORK_PATH}"
-
-rm -rf "${SIMULATOR_ARCHIVE_PATH}"
-rm -rf "${IOS_DEVICE_ARCHIVE_PATH}"
-rm -rf "${CATALYST_ARCHIVE_PATH}"
+create_xcframework "OneSignal_Extension" "OneSignalExtension" "OneSignalExtension"
 
 ## BUILD ONESIGNAL ##
+create_xcframework "OneSignal_XCFramework" "OneSignal" "OneSignalFramework"
 
-FRAMEWORK_FOLDER_NAME="OneSignal_XCFramework"
-
-FRAMEWORK_NAME="OneSignal"
-
-FRAMEWORK_PATH="${WORKING_DIR}/${FRAMEWORK_FOLDER_NAME}/${FRAMEWORK_NAME}.xcframework"
-
-BUILD_SCHEME="OneSignalFramework"
-
-SIMULATOR_ARCHIVE_PATH="${WORKING_DIR}/${FRAMEWORK_FOLDER_NAME}/simulator.xcarchive"
-
-IOS_DEVICE_ARCHIVE_PATH="${WORKING_DIR}/${FRAMEWORK_FOLDER_NAME}/iOS.xcarchive"
-
-CATALYST_ARCHIVE_PATH="${WORKING_DIR}/${FRAMEWORK_FOLDER_NAME}/catalyst.xcarchive"
-
-rm -rf "${WORKING_DIR}/${FRAMEWORK_FOLDER_NAME}"
-echo "Deleted ${FRAMEWORK_FOLDER_NAME}"
-mkdir "${FRAMEWORK_FOLDER_NAME}"
-echo "Created ${FRAMEWORK_FOLDER_NAME}"
-echo "Archiving ${FRAMEWORK_NAME}"
-
-xcodebuild archive ONLY_ACTIVE_ARCH=NO -scheme ${BUILD_SCHEME} -destination="generic/platform=iOS Simulator" -archivePath "${SIMULATOR_ARCHIVE_PATH}" -sdk iphonesimulator SKIP_INSTALL=NO BUILD_LIBRARIES_FOR_DISTRIBUTION=YES
-
-xcodebuild archive -scheme ${BUILD_SCHEME} -destination="generic/platform=iOS" -archivePath "${IOS_DEVICE_ARCHIVE_PATH}" -sdk iphoneos SKIP_INSTALL=NO BUILD_LIBRARIES_FOR_DISTRIBUTION=YES
-
-xcodebuild archive -scheme ${BUILD_SCHEME} -destination='generic/platform=macOS,variant=Mac Catalyst' -archivePath "${CATALYST_ARCHIVE_PATH}" SKIP_INSTALL=NO BUILD_LIBRARIES_FOR_DISTRIBUTION=YES
-
-xcodebuild -create-xcframework -framework ${SIMULATOR_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework -framework ${IOS_DEVICE_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework -framework ${CATALYST_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework -output "${FRAMEWORK_PATH}"
-
-rm -rf "${SIMULATOR_ARCHIVE_PATH}"
-rm -rf "${IOS_DEVICE_ARCHIVE_PATH}"
-rm -rf "${CATALYST_ARCHIVE_PATH}"
 open "${WORKING_DIR}"
