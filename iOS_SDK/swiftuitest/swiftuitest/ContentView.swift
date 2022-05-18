32a0d0c2cd44a602eac8858ae86c22161ee2a179
emawby <emawby@gmail.com>
2022-05-18 12:55:00 -0700

    adding a swiftui app

---
+++ iOS_SDK/swiftuitest/swiftuitest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftuitest
+//
+//  Created by Elliot Mawby on 5/18/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS_SDK/swiftuitest/swiftuitest/swiftuitestApp.swift
@@ -0,0 +1,44 @@
+//
+//  swiftuitestApp.swift
+//  swiftuitest
+//
+//  Created by Elliot Mawby on 5/18/22.
+//
+
+import SwiftUI
+import OneSignal
+
+@main
+struct swiftuitestApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+           // Remove this method to stop OneSignal Debugging
+        OneSignal.setLogLevel(.LL_VERBOSE, visualLevel: .LL_NONE)
+        OneSignal.sendTag("name", value: "elliot")
+        OneSignal.initWithLaunchOptions(launchOptions)
+        OneSignal.setAppId("77e32082-ea27-42e3-a898-c72e141824ef")
+
+        OneSignal.promptForPushNotifications(userResponse: { accepted in
+         print("User accepted notification: \(accepted)")
+        })
+
+        return true
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        print("ecm swizzle test didregister")
+    }
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        print("ecm swizzle test didReceive")
+    }
+}
