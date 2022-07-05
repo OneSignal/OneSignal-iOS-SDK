747f4b772e0b3308226fc182da5fe827dff4885d
emawby <emawby@gmail.com>
2022-07-05 11:01:22 -0700

    successfully running build_all_frameworks

---
+++ iOS_SDK/OneSignalSDK/OneSignal_User/OneSignalUser.xcframework/ios-arm64_i386_x86_64-simulator/OneSignalUser.framework/Headers/OneSignalUser.h
@@ -0,0 +1,19 @@
+//
+//  OneSignalUser.h
+//  OneSignalUser
+//
+//  Created by Elliot Mawby on 5/13/22.
+//  Copyright © 2022 Hiptic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OneSignalUser.
+FOUNDATION_EXPORT double OneSignalUserVersionNumber;
+
+//! Project version string for OneSignalUser.
+FOUNDATION_EXPORT const unsigned char OneSignalUserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OneSignalUser/PublicHeader.h>
+
+//#import <OneSignalUser/OneSignalUser-Swift.h>
