1dd8dd4a4e79d405b1903a0a59f05c3d98d01a63
Nan <nanftw@gmail.com>
2022-07-18 19:56:06 -0700

    [Example App] add testing the methods in Swift
    Add misc. calls to the new stubbed methods and access to `OSUser` in the Dev App via a new Swift file `SwiftTestFile.swift`

---
+++ iOS_SDK/OneSignalDevApp/OneSignalDevApp/SwiftTestFile.swift
@@ -0,0 +1,57 @@
+//
+//  SwiftTestFile.swift
+//  OneSignalExample
+//
+//  Copyright © 2022 OneSignal. All rights reserved.
+//
+
+import Foundation
+import OneSignal
+
+@objc
+public class SwiftTestFile: NSObject {
+    @objc
+    public func testSwift() -> Void {
+        // Testing access of methods and parameters
+        
+        // User Identity
+        var _ = OneSignal.user
+        var _ = OneSignal.login("foo")
+        var _ = OneSignal.login("foo", withToken: "someToken")
+        var _ = OneSignal.loginGuest()
+               
+        // TODO: access user like a property instead of user()
+        
+        // Aliases
+        OneSignal.user().addAlias("foo", id: "bar")
+        OneSignal.user().addAliases(["foo": "foo1", "bar": "bar2"])
+        OneSignal.user().removeAlias("foo")
+        OneSignal.user().removeAliases(["foo", "bar"])
+        
+        // Tags
+        OneSignal.user().setTag("foo", "bar")
+        OneSignal.user().setTags(["foo": "foo1", "bar": "bar2"])
+        OneSignal.user().removeTag("foo")
+        OneSignal.user().removeTags(["foo", "bar"])
+        
+        // Outcomes
+        OneSignal.user().setOutcome("foo")
+        OneSignal.user().setUniqueOutcome("foo")
+        OneSignal.user().setOutcomeWithValue("foo", 4.50)
+        
+        // Email
+        OneSignal.user().addEmail("person@example.com")
+        OneSignal.user().removeEmail("person@example.com")
+
+        // SMS
+        OneSignal.user().addSmsNumber("+15551231234")
+        OneSignal.user().removeSmsNumber("+15551231234")
+        
+        // Triggers
+        OneSignal.user().setTrigger("foo", "bar")
+        OneSignal.user().setTriggers(["foo": "foo1", "bar": "bar2"])
+        OneSignal.user().removeTrigger("foo")
+        OneSignal.user().removeTriggers(["foo", "bar"])
+
+    }
+}
