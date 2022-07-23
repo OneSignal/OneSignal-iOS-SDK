1d203ee770b31d2994177d1050de9a98b4d8d416
Nan <nanftw@gmail.com>
2022-07-22 17:01:25 -0700

    unit tests in Obj-C and Swift
    * Create 2 unit test classes for the user model, one for Obj-C and one for Swift.
    * Place the new public APIs in it to test access of the APIs
    * Please check these files for how the new APIs are called by users
    *Remove user model references from all other files
    
    * For simplicity, remove user model APIs from the Dev App so we don't have to update multiple places when there is a code change.

---
+++ iOS_SDK/OneSignalSDK/UnitTests/UserModelSwiftTests.swift
@@ -0,0 +1,111 @@
+/*
+ Modified MIT License
+
+ Copyright 2022 OneSignal
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ 1. The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ 2. All copies of substantial portions of the Software may only be used in connection
+ with services provided by OneSignal.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ */
+
+import XCTest
+import OneSignal
+
+// TODO: This goes elsewhere
+extension OneSignal {
+    static var user: OSUser {
+        return OneSignal.__user()
+    }
+}
+
+class UserModelSwiftTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        try super.tearDownWithError()
+    }
+
+    func testPropertyWrapper_checkPrintLog() throws {
+        // temp. test to see that OSQueueable's didSet is called on externalId by checking logs
+        var myUser = OneSignal.user
+        OneSignal.login("foo") { user in
+            myUser = user
+        }
+        XCTAssertNotNil(myUser)
+    }
+
+    func testUserModelMethodAccess() throws {
+        // this test lays out the public APIs of the user model
+        
+        // User Identity
+        var myUser: OSUser = OneSignal.user
+
+        OneSignal.login("foo") { user in
+            myUser = user
+        }
+
+        OneSignal.login("foo", withToken: "someToken") { user in
+            myUser = user
+        }
+
+        var _ = OneSignal.loginGuest { user in
+            myUser = user
+        }
+
+        // Aliases
+        OneSignal.user.addAlias(label: "foo", id: "bar")
+        OneSignal.user.addAliases(["foo": "foo1", "bar": "bar2"])
+        OneSignal.user.removeAlias("foo")
+        OneSignal.user.removeAliases(["foo", "bar"])
+
+        // Tags
+        OneSignal.user.setTag(key: "foo", value: "bar")
+        OneSignal.user.setTags(["foo": "foo1", "bar": "bar2"])
+        OneSignal.user.removeTag("foo")
+        OneSignal.user.removeTags(["foo", "bar"])
+        OneSignal.user.getTag("foo")
+
+        // Outcomes
+        OneSignal.user.setOutcome("foo")
+        OneSignal.user.setUniqueOutcome("foo")
+        OneSignal.user.setOutcome(name: "foo", value: 4.50)
+
+        // Email
+        OneSignal.user.addEmail("person@example.com")
+        OneSignal.user.removeEmail("person@example.com")
+
+        // SMS
+        OneSignal.user.addSmsNumber("+15551231234")
+        OneSignal.user.removeSmsNumber("+15551231234")
+
+        // Triggers
+        OneSignal.user.setTrigger(key: "foo", value: "bar")
+        OneSignal.user.setTriggers(["foo": "foo1", "bar": "bar2"])
+        OneSignal.user.removeTrigger("foo")
+        OneSignal.user.removeTriggers(["foo", "bar"])
+
+        XCTAssertNotNil(myUser)
+    }
+}
