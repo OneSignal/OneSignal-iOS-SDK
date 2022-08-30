eb69f7232d279322749f44761d9c600b76459ee4
emawby <emawby@gmail.com>
2022-08-30 12:50:36 -0700

    running swiftlint --fix

--- iOS_SDK/OneSignalSDK/OneSignalUser/Source/OSUser.swift
+++ iOS_SDK/OneSignalSDK/OneSignalUser/Source/OSUser.swift
@@ -30,139 +30,139 @@ import OneSignalCore
 
 @objc
 public class OSUser: NSObject {
-    
+
     let onesignalId: UUID
     var externalId: String?
     var language: String?
-    var aliases: [String : String] = [:]
-    var tags: [String : String] = [:]
-    var triggers: [String : String] = [:] // update to include bool, number
-    
+    var aliases: [String: String] = [:]
+    var tags: [String: String] = [:]
+    var triggers: [String: String] = [:] // update to include bool, number
+
     // email, sms, subscriptions todo
-    
+
     @objc public var pushSubscription: OSPushSubscription
-    
+
     // TODO: UM This is a temporary function to create a push subscription for testing
     @objc public func testCreatePushSubscription(subscriptionId: UUID, token: UUID, enabled: Bool) {
         self.pushSubscription = OSPushSubscription(subscriptionId: subscriptionId, token: token, enabled: enabled)
         print("🔥 OSUser has set pushSubcription for testing")
     }
-    
+
     @objc public init(onesignalId: UUID, pushSubscription: OSPushSubscription) {
         self.onesignalId = onesignalId
         self.pushSubscription = pushSubscription
     }
-    
+
     // Aliases
-    
+
     @objc
-    public func addAlias(label: String, id: String) -> Void {
+    public func addAlias(label: String, id: String) {
         print("🔥 OSUser addAlias() called")
     }
-    
+
     @objc
-    public func addAliases(_ aliases: [String : String]) -> Void {
+    public func addAliases(_ aliases: [String: String]) {
         print("🔥 OSUser addAliases() called")
     }
-    
+
     @objc
-    public func removeAlias(_ label: String) -> Void {
+    public func removeAlias(_ label: String) {
         print("🔥 OSUser removeAlias() called")
     }
-    
+
     @objc
-    public func removeAliases(_ labels: [String]) -> Void {
+    public func removeAliases(_ labels: [String]) {
         print("🔥 OSUser removeAliases() called")
     }
-    
+
     // Tags
-    
+
     @objc
-    public func setTag(key: String, value: String) -> Void {
+    public func setTag(key: String, value: String) {
         print("🔥 OSUser sendTag() called")
     }
-    
+
     @objc
-    public func setTags(_ tags: [String : String]) -> Void {
+    public func setTags(_ tags: [String: String]) {
         print("🔥 OSUser sendTags() called")
     }
-    
+
     @objc
-    public func removeTag(_ tag: String) -> Void {
+    public func removeTag(_ tag: String) {
         print("🔥 OSUser removeTag() called")
     }
-    
+
     @objc
-    public func removeTags(_ tags: [String]) -> Void {
+    public func removeTags(_ tags: [String]) {
         print("🔥 OSUser removeTags() called")
     }
-    
+
     @objc
-    public func getTag(_ tag: String) -> Void {
+    public func getTag(_ tag: String) {
         print("🔥 OSUser getTag() called")
     }
-    
+
     // Outcomes
-    
+
     @objc
-    public func setOutcome(_ name: String) -> Void {
+    public func setOutcome(_ name: String) {
         print("🔥 OSUser sendOutcome() called")
     }
-    
+
     @objc
-    public func setUniqueOutcome(_ name: String) -> Void {
+    public func setUniqueOutcome(_ name: String) {
         print("🔥 OSUser setUniqueOutcome() called")
     }
-    
+
     @objc
-    public func setOutcome(name: String, value: Float) -> Void {
+    public func setOutcome(name: String, value: Float) {
         print("🔥 OSUser setOutcomeWithValue() called")
     }
-    
+
     // Email
-    
+
     @objc
-    public func addEmail(_ email: String) -> Void {
+    public func addEmail(_ email: String) {
         print("🔥 OSUser addEmail() called")
     }
-    
+
     @objc
-    public func removeEmail(_ email: String) -> Void {
+    public func removeEmail(_ email: String) {
         print("🔥 OSUser removeEmail() called")
     }
-    
+
     // SMS
-    
+
     @objc
-    public func addSmsNumber(_ number: String) -> Void {
+    public func addSmsNumber(_ number: String) {
         print("🔥 OSUser addPhoneNumber() called")
     }
-    
+
     @objc
-    public func removeSmsNumber(_ number: String) -> Void {
+    public func removeSmsNumber(_ number: String) {
         print("🔥 OSUser removePhoneNumber() called")
     }
-    
+
     // Triggers
-    
+
     @objc
-    public func setTrigger(key: String, value: String) -> Void {
+    public func setTrigger(key: String, value: String) {
         // TODO: UM Value for trigger can be non-string
         print("🔥 OSUser setTrigger() called")
     }
-    
+
     @objc
-    public func setTriggers(_ triggers: [String : String]) -> Void {
+    public func setTriggers(_ triggers: [String: String]) {
         print("🔥 OSUser setTriggers() called")
     }
-    
+
     @objc
-    public func removeTrigger(_ trigger: String) -> Void {
+    public func removeTrigger(_ trigger: String) {
         print("🔥 OSUser removeTrigger() called")
     }
-    
+
     @objc
-    public func removeTriggers(_ triggers: [String]) -> Void {
+    public func removeTriggers(_ triggers: [String]) {
         print("🔥 OSUser removeTriggers() called")
     }
 }
