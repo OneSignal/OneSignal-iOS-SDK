5668dcec91b1f53d536485d24065d2d37902f6bf
Nan <nanftw@gmail.com>
2022-09-06 11:00:40 -0400

    updates to caching deltas and operations
    * For adding and removing operations and deltas from UserDefaults (cache), save the entire updated list of them as the value instead of individual deltas or operations.

--- iOS_SDK/OneSignalSDK/OneSignalUser/Source/OSPropertyOperationExecutor.swift
+++ iOS_SDK/OneSignalSDK/OneSignalUser/Source/OSPropertyOperationExecutor.swift
@@ -51,7 +51,7 @@ class OSPropertyOperationExecutor: OSOperationExecutor {
         // TODO: Implementation
         for delta in deltaQueue {
             // Remove the delta from the cache when it becomes an Operation
-            OneSignalUserDefaults.initShared().removeValue(forKey: delta.deltaId.uuidString)
+            OSOperationRepo.sharedInstance.removeDeltaFromCache(delta)
             // enqueueOperation(operation)
         }
         processOperationQueue()
@@ -59,9 +59,10 @@ class OSPropertyOperationExecutor: OSOperationExecutor {
     
     func enqueueOperation(_ operation: OSOperation) {
         print("🔥 OSPropertyOperationExecutor enqueueOperation: \(operation)")
-        // Cache the Operation
-        OneSignalUserDefaults.initShared().saveObject(forKey: operation.operationId.uuidString, withValue: operation)
         operationQueue.append(operation)
+
+        // persist executor's operations (including new operation) to storage
+        OneSignalUserDefaults.initShared().saveCodeableData(forKey: "OS_PROPERTY_OPERATION_EXECUTOR_OPERATIONS", withValue: self.operationQueue)
     }
     
     func processOperationQueue() {
@@ -80,7 +81,7 @@ class OSPropertyOperationExecutor: OSOperationExecutor {
         let response = ["language": "en"]
 
         // On success, remove operation from cache, and hydrate model
-        OneSignalUserDefaults.initShared().removeValue(forKey: operation.operationId.uuidString)
+        OneSignalUserDefaults.initShared().saveCodeableData(forKey: "OS_PROPERTY_OPERATION_EXECUTOR_OPERATIONS", withValue: self.operationQueue)
         
         operation.model.hydrate(response)
         
