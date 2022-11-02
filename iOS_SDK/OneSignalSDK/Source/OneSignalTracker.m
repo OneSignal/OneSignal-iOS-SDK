6caf2227aa00f7e158dcc792873f5dbe88b27749
emawby <emawby@gmail.com>
2022-11-02 16:22:25 -0700

    adding delete comments for sending badge counts to server

--- iOS_SDK/OneSignalSDK/Source/OneSignalTracker.m
+++ iOS_SDK/OneSignalSDK/Source/OneSignalTracker.m
@@ -155,7 +155,7 @@ static BOOL didEnterBackgroundTriggered = NO;
         return;
     
     // If badge was set, clear it on the server as well.
-    if (wasBadgeSet) //TODO: Move to Notifications
+    if (wasBadgeSet) //TODO: Delete with um
         [OneSignal.stateSynchronizer sendBadgeCount:@0 appId:[OneSignal appId]];
 }
 
