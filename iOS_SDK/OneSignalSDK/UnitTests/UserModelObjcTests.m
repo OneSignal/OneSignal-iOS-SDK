3b6acc84819300d1283aa6032bc5bde5de69c03f
Nan <nanftw@gmail.com>
2022-12-05 01:15:53 -0800

    Add `OneSignalInAppMessaging` class
    * It will implement the namespace `InAppMessages`
    * The protocol for the namespace is `OSInAppMessages`
    * Triggers and IAM handlers are on it

--- iOS_SDK/OneSignalSDK/UnitTests/UserModelObjcTests.m
+++ iOS_SDK/OneSignalSDK/UnitTests/UserModelObjcTests.m
@@ -94,10 +94,11 @@
     [OneSignal.User removeSmsNumber:@"+15551231234"];
 
     // Triggers
-    [OneSignal.User setTriggerWithKey:@"foo" value:@"bar"];
-    [OneSignal.User setTriggers:@{@"foo": @"foo1", @"bar": @"bar2"}];
-    [OneSignal.User removeTrigger:@"foo"];
-    [OneSignal.User removeTriggers:@[@"foo", @"bar"]];
+    [OneSignal.InAppMessages addTrigger:@"foo" withValue:@"bar"];
+    [OneSignal.InAppMessages addTriggers:@{@"foo": @"foo1", @"bar": @"bar2"}];
+    [OneSignal.InAppMessages removeTriggerForKey:@"foo"];
+    [OneSignal.InAppMessages removeTriggersForKeys:@[@"foo", @"bar"]];
+    [OneSignal.InAppMessages clearTriggers];
 }
 
 /**
