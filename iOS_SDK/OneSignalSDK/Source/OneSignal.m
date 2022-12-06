caf6be118cd59256246c79eae4c1d0bcf2014d54
Nan <nanftw@gmail.com>
2022-12-05 21:31:54 -0800

    make changes to some NS_REFINED_FOR_SWIFT
    * Session namespace needs this
    * User namespace needs to change from returning a class to returning an instance after changing `OneSignalUserManagerImpl`  from a static class to a to be a `sharedInstance`

--- iOS_SDK/OneSignalSDK/Source/OneSignal.m
+++ iOS_SDK/OneSignalSDK/Source/OneSignal.m
@@ -250,7 +250,7 @@ static AppEntryAction _appEntryState = APP_CLOSE;
 
 #pragma mark User Model - User Identity 🔥
 
-+ (Class<OSUser>)User {
++ (id<OSUser>)User {
     return [OneSignalUserManagerImpl.sharedInstance User];
 }
 
