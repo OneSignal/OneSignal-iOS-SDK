bcb3b68dea3dbf1568e68b120116c07daa015eb1
emawby <emawby@gmail.com>
2022-12-12 13:40:12 -0800

    Testing refined for swift using objc swift interface

---
+++ iOS_SDK/OneSignalDevApp/OneSignalDevApp/SwiftTest.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftTest.swift
+//  OneSignalExample
+//
+//  Created by Elliot Mawby on 12/12/22.
+//  Copyright © 2022 OneSignal. All rights reserved.
+//
+
+import Foundation
+import OneSignal
+
+class SwiftTest: NSObject {
+    func testSwiftUserModel() {
+        //OneSignal.user()
+        OneSignal.User.onJwtExpired { externalId, completion in
+            completion("test")
+        }
+        let token1 = OneSignal.User.pushSubscription.token
+        let token = OneSignal.User.pushSubscription.token
+    }
+}
