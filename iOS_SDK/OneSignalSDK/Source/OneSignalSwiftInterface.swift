870a066c90b3daf9fd1c39011f3693fd640559ac
emawby <emawby@gmail.com>
2022-12-12 13:40:12 -0800

    Testing refined for swift using objc swift interface

---
+++ iOS_SDK/OneSignalSDK/Source/OneSignalSwiftInterface.swift
@@ -0,0 +1,37 @@
+//
+//  OneSignalSwiftInterface.swift
+//  OneSignal
+//
+//  Created by Elliot Mawby on 12/12/22.
+//  Copyright © 2022 Hiptic. All rights reserved.
+//
+
+import Foundation
+
+typealias OneSignalSwift = OneSignal
+
+public extension OneSignalSwift {
+    static var User: OSUser {
+        return __user()
+    }
+    
+    static var Notifications: OSNotifications.Type {
+        return __notifications()
+    }
+    
+    static var Session: OSSession.Type {
+        return __session()
+    }
+    
+    static var InAppMessages: OSInAppMessages.Type {
+        return __inAppMessages()
+    }
+    
+    static var Debug: OSDebug.Type {
+        return __debug()
+    }
+    
+    static var Location: OSLocation.Type {
+        return __location()
+    }
+}
