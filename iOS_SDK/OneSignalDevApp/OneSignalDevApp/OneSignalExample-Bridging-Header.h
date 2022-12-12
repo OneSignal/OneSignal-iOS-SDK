1eb32de87e100f54b453707813cb7f2338066c54
emawby <emawby@gmail.com>
2022-12-12 13:40:12 -0800

    Testing refined for swift using objc swift interface

---
+++ iOS_SDK/OneSignalDevApp/OneSignalDevApp/OneSignalExample-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  OneSignalExample-Bridging-Header.h
+//  OneSignalExample
+//
+//  Created by Elliot Mawby on 12/12/22.
+//  Copyright © 2022 OneSignal. All rights reserved.
+//
+
+#ifndef OneSignalExample_Bridging_Header_h
+#define OneSignalExample_Bridging_Header_h
+
+
+#endif /* OneSignalExample_Bridging_Header_h */
