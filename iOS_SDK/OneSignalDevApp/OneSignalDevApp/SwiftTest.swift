d8f152400ea1eb71bf91d2c292ce5f7d5a16dbb6
emawby <emawby@gmail.com>
2022-12-12 17:49:14 -0800

    fixing app clip

--- iOS_SDK/OneSignalDevApp/OneSignalDevApp/SwiftTest.swift
+++ iOS_SDK/OneSignalDevApp/OneSignalDevApp/SwiftTest.swift
@@ -7,15 +7,15 @@
 //
 
 import Foundation
-//import OneSignalFramework
-//
-//class SwiftTest: NSObject {
-//    func testSwiftUserModel() {
-//        //OneSignal.user()
-//        OneSignal.User.onJwtExpired { externalId, completion in
-//            completion("test")
-//        }
-//        let token1 = OneSignal.User.pushSubscription.token
-//        let token = OneSignal.User.pushSubscription.token
-//    }
-//}
+import OneSignalFramework
+
+class SwiftTest: NSObject {
+    func testSwiftUserModel() {
+        //OneSignal.user()
+        OneSignal.User.onJwtExpired { externalId, completion in
+            completion("test")
+        }
+        let token1 = OneSignal.User.pushSubscription.token
+        let token = OneSignal.User.pushSubscription.token
+    }
+}
