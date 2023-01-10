94da3c0ba385ce397976fa86321f54fc73b711d1
emawby <emawby@gmail.com>
2023-01-10 10:38:11 -0800

    Binaries for Release 5.0.0-alpha-01

--- iOS_SDK/OneSignalSDK/OneSignal_Outcomes/OneSignalOutcomes.xcframework/ios-arm64_x86_64-simulator/OneSignalOutcomes.framework/Headers/OneSignalOutcomes.h
+++ iOS_SDK/OneSignalSDK/OneSignal_Outcomes/OneSignalOutcomes.xcframework/ios-arm64_x86_64-simulator/OneSignalOutcomes.framework/Headers/OneSignalOutcomes.h
@@ -40,8 +40,23 @@
 #import "OSOutcomeEventsFactory.h"
 #import "OSTrackerFactory.h"
 #import "OSOutcomeEventsRepository.h"
+#import "OSFocusInfluenceParam.h"
 
-@interface OneSignalOutcomes : NSObject
+/**
+ Public API for Session namespace.
+ */
+@protocol OSSession <NSObject>
++ (void)addOutcome:(NSString * _Nonnull)name;
++ (void)addUniqueOutcome:(NSString * _Nonnull)name;
++ (void)addOutcomeWithValue:(NSString * _Nonnull)name value:(NSNumber * _Nonnull)value NS_REFINED_FOR_SWIFT;
+
+@end
+
+@interface OSOutcomes : NSObject <OSSession>
++ (Class<OSSession> _Nonnull)Session;
++ (OneSignalOutcomeEventsController * _Nullable)sharedController;
++ (void)start;
++ (void)clearStatics;
 + (void)migrate;
 @end
 
