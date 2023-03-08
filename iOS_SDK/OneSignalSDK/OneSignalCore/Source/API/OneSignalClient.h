d30585528ae13cd792ab2730071757f2ed5f810b
Nan <nanftw@gmail.com>
2023-03-08 12:36:23 -0800

    [cleanup] remove unused methods from OneSignalClient
    Looks like these methods are no longer used within the SDK, so let's remove them from OneSignalClient:
    - executeSimultaneousRequests
    - executeSynchronousRequest
    - executeDataRequest
    
    Update OneSignalClient.h
    
    And types:
    - OSDataRequestSuccessBlock
    - OSMultipleCompletionBlock
    - OSMultipleFailureBlock
    - OSMultipleSuccessBlock
    
    - genericTimedOutError

--- iOS_SDK/OneSignalSDK/OneSignalCore/Source/API/OneSignalClient.h
+++ iOS_SDK/OneSignalSDK/OneSignalCore/Source/API/OneSignalClient.h
@@ -31,52 +31,9 @@
 #ifndef OneSignalClient_h
 #define OneSignalClient_h
 
-typedef void (^OSDataRequestSuccessBlock)(NSData *data);
-
-typedef void (^OSMultipleCompletionBlock)(NSDictionary *responses);
-typedef void (^OSMultipleFailureBlock)(NSDictionary<NSString *, NSError *> *errors);
-typedef void (^OSMultipleSuccessBlock)(NSDictionary<NSString *, NSDictionary *> *results);
-
 @interface OneSignalClient : NSObject
 + (OneSignalClient *)sharedClient;
 - (void)executeRequest:(OneSignalRequest *)request onSuccess:(OSResultSuccessBlock)successBlock onFailure:(OSFailureBlock)failureBlock;
-- (void)executeSynchronousRequest:(OneSignalRequest *)request onSuccess:(OSResultSuccessBlock)successBlock onFailure:(OSFailureBlock)failureBlock;
-
-// ie. for loading HTML or other non-JSON based requests
-- (void)executeDataRequest:(OneSignalRequest *)request onSuccess:(OSDataRequestSuccessBlock)successBlock onFailure:(OSFailureBlock)failureBlock;
-
-// Executes multiple OneSignalRequest's simultaneously, needs a unique identifier for each request
-- (void)executeSimultaneousRequests:(NSDictionary<NSString *, OneSignalRequest *> *)requests withSuccess:(OSMultipleSuccessBlock)successBlock onFailure:(OSMultipleFailureBlock)failureBlock;
-
-/*
- TODO: We want to eventually migrate over to using this method for executing simultaneous requests:
-  This allows us to combine multiple async concurrent requests to return from a single callback with the proper formatted responses from each reuqest (successful or not, account for params returning from GETs).
-  A generalized format should be followed and we should make sure not to break form that as it could break peoples apps in the future if we add params and remove params from this callback.
-  Currently for the only implementation this is used for "setExternalUserId:withCOmpletion:" the format is as follows:
-  
-     NSDictionary response = @{
-        (required) @"push" : {
-            @"success" : @(true) or @(false)
-        },
-        
-        (optional) @"email" : {
-            @"success" : @(true) or @(false)
-        }
-     }
-
- 
-  Building off of this format now will require:
- 
-    1. Including other attributes and whether they are required or not
-        ex. @"push" is always going to be within the callback resposne (required), meanwhile,
-            @"email" will not always exist in the callback resposne (optoinal)
-    
-    2. Can't remove params that are required as an app may be expecting them and removing/modifying a key could break there app with an SDK upgrade
-    
-    3. Add more requirements...
- 
- */
-- (void)executeSimultaneousRequests:(NSDictionary<NSString *, OneSignalRequest *> *)requests withCompletion:(OSMultipleCompletionBlock)completionBlock;
 @end
 
 #endif
