2e79dc9676598d67884be4ef39cb5a9c8813d0ff
emawby <emawby@gmail.com>
2023-05-01 13:54:10 -0700

    bumping version numbers for 5.0.0 beta 3

--- iOS_SDK/OneSignalSDK/OneSignalCore/Source/OneSignalCommonDefines.h
+++ iOS_SDK/OneSignalSDK/OneSignalCore/Source/OneSignalCommonDefines.h
@@ -46,7 +46,7 @@
 // "*" in comment line ending comment means the string value has not been changed
 // App
 
-#define ONESIGNAL_VERSION                                                   @"050000-beta-02"
+#define ONESIGNAL_VERSION                                                   @"050000-beta-03"
 
 #define OSUD_APP_ID                                                         @"GT_APP_ID"                                                        // * OSUD_APP_ID
 #define OSUD_REGISTERED_WITH_APPLE                                          @"GT_REGISTERED_WITH_APPLE"                                         // * OSUD_REGISTERED_WITH_APPLE
