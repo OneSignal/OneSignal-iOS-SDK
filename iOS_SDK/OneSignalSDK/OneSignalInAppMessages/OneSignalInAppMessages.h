c4aeb938ea90c19cdd7e9e1b4c51fd01f9307d4f
emawby <emawby@gmail.com>
2023-06-20 10:02:59 -0700

    Create blank OneSignalInAppMessages framework

---
+++ iOS_SDK/OneSignalSDK/OneSignalInAppMessages/OneSignalInAppMessages.h
@@ -0,0 +1,19 @@
+//
+//  OneSignalInAppMessages.h
+//  OneSignalInAppMessages
+//
+//  Created by Elliot Mawby on 6/20/23.
+//  Copyright © 2023 Hiptic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OneSignalInAppMessages.
+FOUNDATION_EXPORT double OneSignalInAppMessagesVersionNumber;
+
+//! Project version string for OneSignalInAppMessages.
+FOUNDATION_EXPORT const unsigned char OneSignalInAppMessagesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OneSignalInAppMessages/PublicHeader.h>
+
+
