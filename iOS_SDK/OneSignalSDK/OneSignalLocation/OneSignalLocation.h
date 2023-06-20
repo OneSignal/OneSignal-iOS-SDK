172fc6ba175574fd204b52c26c48c0b7c414ae4e
emawby <emawby@gmail.com>
2023-06-20 09:34:06 -0700

    Create blank OneSignalLocation framework

---
+++ iOS_SDK/OneSignalSDK/OneSignalLocation/OneSignalLocation.h
@@ -0,0 +1,19 @@
+//
+//  OneSignalLocation.h
+//  OneSignalLocation
+//
+//  Created by Elliot Mawby on 6/20/23.
+//  Copyright © 2023 Hiptic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OneSignalLocation.
+FOUNDATION_EXPORT double OneSignalLocationVersionNumber;
+
+//! Project version string for OneSignalLocation.
+FOUNDATION_EXPORT const unsigned char OneSignalLocationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OneSignalLocation/PublicHeader.h>
+
+
