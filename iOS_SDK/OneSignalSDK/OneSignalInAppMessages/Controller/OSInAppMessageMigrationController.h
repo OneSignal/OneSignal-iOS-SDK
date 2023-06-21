1cf93d20624868f0722b2cfcfae6b96c4579932d
emawby <emawby@gmail.com>
2023-06-21 12:14:24 -0700

    Migrate IAMs in the IAM framework
    The Migration controller will now tell the OneSignalInAppMessages framework to do its migration. This way the MigrationController doesn't need to reference internal IAM classes.

---
+++ iOS_SDK/OneSignalSDK/OneSignalInAppMessages/Controller/OSInAppMessageMigrationController.h
@@ -0,0 +1,14 @@
+//
+//  OSInAppMessageMigrationController.h
+//  OneSignalInAppMessages
+//
+//  Created by Elliot Mawby on 6/21/23.
+//  Copyright © 2023 Hiptic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OSInAppMessageMigrationController : NSObject
++ (void)migrate;
+@end
+
