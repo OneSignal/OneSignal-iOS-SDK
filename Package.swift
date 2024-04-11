62b3017c375d35d2aaa8a464f5dc27cc8313083c
Nan <nanftw@gmail.com>
2024-04-11 16:29:31 -0700

    Update Package.swift + script
    * Add `OneSignalLiveActivities` to the OneSignalFramework
    * Add `OneSignalLiveActivities` to the update_swift_package script

--- Package.swift
+++ Package.swift
@@ -26,6 +26,7 @@ let package = Package(
                 "OneSignalFramework",
                 "OneSignalUser",
                 "OneSignalNotifications",
+                "OneSignalLiveActivities",
                 "OneSignalExtension",
                 "OneSignalOutcomes",
                 "OneSignalOSCore",
