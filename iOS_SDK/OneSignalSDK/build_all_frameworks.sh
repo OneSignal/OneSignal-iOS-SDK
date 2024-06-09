b105eba1053ab2a8fa52903147bffe7cbac3bac7
Nan <nanftw@gmail.com>
2024-06-09 16:54:26 -0700

    Update build script so OneSignalLiveActivities has mac catalyst
    * This change is needed to build the SDK for distribution or else fail to archive

--- iOS_SDK/OneSignalSDK/build_all_frameworks.sh
+++ iOS_SDK/OneSignalSDK/build_all_frameworks.sh
@@ -39,38 +39,6 @@ create_xcframework() {
     rm -rf "${CATALYST_ARCHIVE_PATH}"
 }
 
-create_xcframework_no_catalyst() {
-    FRAMEWORK_FOLDER_NAME=$1
-
-    FRAMEWORK_NAME=$2
-
-    FRAMEWORK_PATH="${WORKING_DIR}/${FRAMEWORK_FOLDER_NAME}/${FRAMEWORK_NAME}.xcframework"
-
-    BUILD_SCHEME=$3
-
-    SIMULATOR_ARCHIVE_PATH="${WORKING_DIR}/${FRAMEWORK_FOLDER_NAME}/simulator.xcarchive"
-
-    IOS_DEVICE_ARCHIVE_PATH="${WORKING_DIR}/${FRAMEWORK_FOLDER_NAME}/iOS.xcarchive"
-
-    rm -rf "${WORKING_DIR}/${FRAMEWORK_FOLDER_NAME}"
-    echo "Deleted ${FRAMEWORK_FOLDER_NAME}"
-    mkdir "${FRAMEWORK_FOLDER_NAME}"
-    echo "Created ${FRAMEWORK_FOLDER_NAME}"
-    echo "Archiving ${FRAMEWORK_NAME}"
-
-    xcodebuild -list
-
-    xcodebuild archive ONLY_ACTIVE_ARCH=NO -scheme ${BUILD_SCHEME} -destination="generic/platform=iOS Simulator" -archivePath "${SIMULATOR_ARCHIVE_PATH}" -sdk iphonesimulator SKIP_INSTALL=NO BUILD_LIBRARIES_FOR_DISTRIBUTION=YES
-
-    xcodebuild archive -scheme ${BUILD_SCHEME} -destination="generic/platform=iOS" -archivePath "${IOS_DEVICE_ARCHIVE_PATH}" -sdk iphoneos SKIP_INSTALL=NO BUILD_LIBRARIES_FOR_DISTRIBUTION=YES
-
-    xcodebuild -create-xcframework -framework ${SIMULATOR_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework -framework ${IOS_DEVICE_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework -output "${FRAMEWORK_PATH}"
-
-    rm -rf "${SIMULATOR_ARCHIVE_PATH}"
-    rm -rf "${IOS_DEVICE_ARCHIVE_PATH}"
-    rm -rf "${CATALYST_ARCHIVE_PATH}"
-}
-
 # BUILD ONESIGNAL CORE ##
 create_xcframework "OneSignal_Core" "OneSignalCore" "OneSignalCore"
 
@@ -90,7 +58,7 @@ create_xcframework "OneSignal_Notifications" "OneSignalNotifications" "OneSignal
 create_xcframework "OneSignal_User" "OneSignalUser" "OneSignalUser"
 
 ## BUILD ONESIGNAL LIVE ACTIVITIES ##
-create_xcframework_no_catalyst "OneSignal_LiveActivities" "OneSignalLiveActivities" "OneSignalLiveActivities"
+create_xcframework "OneSignal_LiveActivities" "OneSignalLiveActivities" "OneSignalLiveActivities"
 
 ## BUILD ONESIGNAL USER ##
 create_xcframework "OneSignal_Location" "OneSignalLocation" "OneSignalLocation"
