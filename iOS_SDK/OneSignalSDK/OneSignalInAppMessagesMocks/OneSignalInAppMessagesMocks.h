a034cf051b639ce59b87c1bfc2819e7f2309fe02
Nan <nan@onesignal.com>
2024-12-10 10:28:59 -0800

    Add module OneSignalInAppMessagesMocks

---
+++ iOS_SDK/OneSignalSDK/OneSignalInAppMessagesMocks/OneSignalInAppMessagesMocks.h
@@ -0,0 +1,19 @@
+//
+//  OneSignalInAppMessagesMocks.h
+//  OneSignalInAppMessagesMocks
+//
+//  Created by Nan Li on 12/10/24.
+//  Copyright © 2024 Hiptic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OneSignalInAppMessagesMocks.
+FOUNDATION_EXPORT double OneSignalInAppMessagesMocksVersionNumber;
+
+//! Project version string for OneSignalInAppMessagesMocks.
+FOUNDATION_EXPORT const unsigned char OneSignalInAppMessagesMocksVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OneSignalInAppMessagesMocks/PublicHeader.h>
+
+
