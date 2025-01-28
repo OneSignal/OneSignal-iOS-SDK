2dfbb326bd361fa4f6d410b91ad667dbe8029bd6
Nan <nan@onesignal.com>
2025-01-28 10:15:17 -0800

    Add / migrate some existing IAM integration tests
    * Attempt to migrate `testDisablingIAMs_stillCreatesMessageQueue_butPreventsMessageDisplay` to new tests, but behavior has changed. It becomes `testPausingIAMs_doesNotCreateMessageQueue`
    * Migrate `testPreviewIAMIsDisplayedOnPause`

--- iOS_SDK/OneSignalSDK/OneSignalInAppMessagesTests/IAMIntegrationTests.swift
+++ iOS_SDK/OneSignalSDK/OneSignalInAppMessagesTests/IAMIntegrationTests.swift
@@ -27,17 +27,89 @@ with services provided by OneSignal.
 
 import XCTest
 @testable import OneSignalInAppMessages
+import OneSignalOSCore
+import OneSignalUser
+import OneSignalCoreMocks
+import OneSignalOSCoreMocks
+import OneSignalUserMocks
+import OneSignalInAppMessagesMocks
 
+/**
+ These tests can include some Obj-C InAppMessagingIntegrationTests migrations.
+ */
 final class IAMIntegrationTests: XCTestCase {
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-    func testExample() throws {
+        OneSignalCoreMocks.clearUserDefaults()
+        OneSignalUserMocks.reset()
+        OSConsistencyManager.shared.reset()
+        // Temp. logging to help debug during testing
         OneSignalLog.setLogLevel(.LL_VERBOSE)
-        OneSignalInAppMessages.getFromServer("foobar")
-        // OSMessagingController.sharedInstance()
+    }
+
+    override func tearDownWithError() throws { }
+
+    /**
+     Test IAMs should display even when IAMs are paused.
+     */
+    func testPreviewIAMIsDisplayedOnPause() throws {
+        /* Setup */
+        OneSignalCoreImpl.setSharedClient(MockOneSignalClient())
+        // App ID is set because there are guards against nil App ID
+        OneSignalConfigManager.setAppId("test-app-id")
+
+        // 1. Pause IAMs
+        OneSignalInAppMessages.__paused(true)
+
+        // 2. Create a preview message
+        let messageJson = IAMTestHelpers.testMessagePreviewJson()
+        let message = OSInAppMessageInternal.instance(withJson: messageJson)
+        XCTAssertNotNil(message, "Preview message should be created successfully")
+
+        // 3. Present the preview message
+        OSMessagingController.sharedInstance().present(inAppPreviewMessage: message)
+
+        // 4. Verify that the preview IAM is showing even when paused
+        XCTAssertTrue(OSMessagingController.sharedInstance().isInAppMessageShowing)
+    }
+
+    /**
+     Pausing IAMs will not evaluate messages.
+     */
+    func testPausingIAMs_doesNotCreateMessageQueue() throws {
+        /* Setup */
+
+        let client = MockOneSignalClient()
+        OneSignalCoreImpl.setSharedClient(client)
+
+        // 1. App ID is set because there are guards against nil App ID
+        OneSignalConfigManager.setAppId("test-app-id")
+
+        // 2. Set up mock responses for the anonymous user, as the user needs an OSID
+        MockUserRequests.setDefaultCreateAnonUserResponses(with: client)
+
+        // 3. Set up mock responses for fetching IAMs
+        let message = IAMTestHelpers.testMessageJsonWithTrigger(property: "session_time", triggerId: "test_id1", type: 1, value: 10.0)
+        let response = IAMTestHelpers.testFetchMessagesResponse(messages: [message])
+        client.setMockResponseForRequest(
+            request: "<OSRequestGetInAppMessages from apps/test-app-id/subscriptions/\(testPushSubId)/iams>",
+            response: response)
+
+        // 4. Unblock the Consistency Manager to allow fetching of IAMs
+        ConsistencyManagerTestHelpers.setDefaultRywToken(id: anonUserOSID)
+
+        // 5. Pausing should prevent messages from being evaluated and shown
+        OneSignalInAppMessages.__paused(true)
+
+        // 6. Start the user manager to generate a user instance
+        OneSignalUserManagerImpl.sharedInstance.start()
+        OneSignalCoreMocks.waitForBackgroundThreads(seconds: 0.5)
+
+        // 7. Fetch IAMs
+        OneSignalInAppMessages.getFromServer(testPushSubId)
+        OneSignalCoreMocks.waitForBackgroundThreads(seconds: 0.5)
+
+        // Make sure no IAM is showing, and the queue has no IAMs
+        XCTAssertFalse(OSMessagingController.sharedInstance().isInAppMessageShowing)
+        XCTAssertEqual(OSMessagingController.sharedInstance().messageDisplayQueue.count, 0)
     }
 }
