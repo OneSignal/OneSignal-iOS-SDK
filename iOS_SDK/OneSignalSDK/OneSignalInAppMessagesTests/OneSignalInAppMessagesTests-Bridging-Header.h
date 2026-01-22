4cd77609793048997db7700f42f426b6d339b9ad
Nan <nan@onesignal.com>
2026-01-21 22:46:19 -0800

    test: add tests for early trigger tracking feature
    Add comprehensive test coverage for the early trigger tracking feature that enables IAMs to display when triggers are added before the first fetch completes on cold start.

--- iOS_SDK/OneSignalSDK/OneSignalInAppMessagesTests/OneSignalInAppMessagesTests-Bridging-Header.h
+++ iOS_SDK/OneSignalSDK/OneSignalInAppMessagesTests/OneSignalInAppMessagesTests-Bridging-Header.h
@@ -8,7 +8,12 @@
 
 // Expose private properties and methods for testing
 @interface OSMessagingController (Testing)
+@property (nonatomic) BOOL hasCompletedFirstFetch;
 @property (strong, nonatomic, nonnull) NSMutableArray <OSInAppMessageInternal *> *messageDisplayQueue;
+@property (strong, nonatomic, nonnull) NSMutableSet<NSString *> *earlySessionTriggers;
+@property (strong, nonatomic, nonnull) NSMutableDictionary *redisplayedInAppMessages;
+@property (strong, nonatomic, nonnull) NSMutableArray <OSInAppMessageInternal *> *messages;
+@property (strong, nonatomic, nonnull) OSTriggerController *triggerController;
 + (void)start;
 + (void)removeInstance;
 - (void)presentInAppPreviewMessage:(OSInAppMessageInternal *)message;
