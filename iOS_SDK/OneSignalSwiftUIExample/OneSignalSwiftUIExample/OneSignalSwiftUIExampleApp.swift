4fb811fd1ea9490cb2a9d7a707878a43067798d8
Nan <nan@onesignal.com>
2026-02-04 12:17:19 -0800

    initial commit of new example app OneSignalSwiftUIExample

---
+++ iOS_SDK/OneSignalSwiftUIExample/OneSignalSwiftUIExample/OneSignalSwiftUIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneSignalSwiftUIExampleApp.swift
+//  OneSignalSwiftUIExample
+//
+//  Created by Nan Li on 2/4/26.
+//
+
+import SwiftUI
+
+@main
+struct OneSignalSwiftUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
