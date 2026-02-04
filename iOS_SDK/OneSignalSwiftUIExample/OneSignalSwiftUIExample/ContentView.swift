a9a4067b410d8195085a5094b03a35f2877dac50
Nan <nan@onesignal.com>
2026-02-04 12:17:19 -0800

    initial commit of new example app OneSignalSwiftUIExample

---
+++ iOS_SDK/OneSignalSwiftUIExample/OneSignalSwiftUIExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OneSignalSwiftUIExample
+//
+//  Created by Nan Li on 2/4/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
