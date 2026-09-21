836b340c4eb6246bfbc65ea18a56ad002b23e2f4
Nan <nanftw@gmail.com>
2026-09-21 10:55:11 -0700

    chore: [SDK-5282] keep the JWT out of the log pipeline (#1747)

--- iOS_SDK/OneSignalSDK/OneSignalCoreTests/OneSignalCoreTests-Bridging-Header.h
+++ iOS_SDK/OneSignalSDK/OneSignalCoreTests/OneSignalCoreTests-Bridging-Header.h
@@ -2,3 +2,10 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+#import <OneSignalCore/OneSignalClient.h>
+
+/// The client's log sites, exposed so tests can read what they print without a network round trip.
+@interface OneSignalClient (LoggingTests)
+- (BOOL)validRequest:(OneSignalRequest *)request;
+- (void)handleJSONNSURLResponse:(NSURLResponse *)response data:(NSData *)data error:(NSError *)error isAsync:(BOOL)async withRequest:(OneSignalRequest *)request onSuccess:(OSResultSuccessBlock)successBlock onFailure:(OSClientFailureBlock)failureBlock;
+@end
